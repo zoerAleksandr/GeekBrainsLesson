50640ad1492d934a5f5bd3582916f6e93b5c857a
Aleksandr <zoer.aleksandr@gmail.com>
2021-04-08 22:28:39 +0300

    Create practice #11

---
+++ src/main/java/com/zoerAleksandr/lesson11/food/Apple.java
@@ -0,0 +1,15 @@
+package com.zoerAleksandr.lesson11.food;
+
+/**
+ * Project GeekBrainsLesson
+ *
+ * @Author Zoer Aleksandr
+ * Created 07.04.2021 18:07
+ */
+public class Apple extends Fruit {
+
+    @Override
+    public float getWeight() {
+        return 1.0f;
+    }
+}
